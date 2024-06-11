83892d5bad984d4528188966a48752d31e5ad58a
Isuru Fernando <isuruf@gmail.com>
2024-06-11 18:55:24 -0500

    Fix for build host merge

--- recipe/copy.sh
+++ recipe/copy.sh
@@ -3,13 +3,7 @@
 HOST=x86_64-w64-mingw32
 export PATH=$SRC_DIR/cf-compilers/bin:$PATH
 
-if [[ "$target_platform" == "win-64" ]]; then
-  INSTALL_PREFIX=${PREFIX}/Library/${HOST}/sysroot/usr
-  make install-strip
-  mkdir -p ${INSTALL_PREFIX}/lib
-  cp libssp*.a ${INSTALL_PREFIX}/lib/
-  cp -r ${INSTALL_PREFIX} $PREFIX/Library/ucrt64
-else
+if [[ "$target_platform" != "win-64" ]]; then
   mkdir -p ${PREFIX}/${HOST}/sysroot
   ln -sf ${PREFIX}/${HOST}/sysroot/usr ${PREFIX}/${HOST}/sysroot/ucrt64
 fi
